6291944280903553a392a66decceed53895e0f70
Josh Man <joshuamangerino@gmail.com>
2022-08-08 14:40:23 -0600

    forgot to add async to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
 });
 
 // find a single tag by its `id`
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       include: {
@@ -44,7 +44,7 @@ router.get('/:id', (req, res) => {
 });
 
 // create a new tag
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -54,7 +54,7 @@ router.post('/', (req, res) => {
 });
 
 // update a tag's name by its `id` value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.update(req.body, {
       where: {
@@ -74,7 +74,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete on tag by its `id` value
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {
